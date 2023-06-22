b6d20bc018052a024897db8ce2365e619de577d2
Tantannn <tango.11@yahoo.com>
2023-06-22 15:28:57 +0700

    add player

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pacman-game
+Making a simple Pacman game
+
--- index.js
+++ index.js
@@ -2,23 +2,6 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
-var img = document.createElement("img");
-document.getElementById("image-container").appendChild(img);
-
-class Boundary {
-    static width = 40
-    static height = 40
-    constructor({position}){
-        this.position = position
-        this.width = 40
-        this.height = 40
-    }
-
-    draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
 
 const map = [
   ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
@@ -36,7 +19,49 @@ const map = [
   ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
 ]
 
+class Boundary {
+  static width = 40
+  static height = 40
+  constructor({position}){
+    this.position = position
+    this.width = 40
+    this.height = 40
+  }
+  draw() {
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+class Player {
+  constructor({position,velocity}){
+    this.position = position
+    this.velocity = velocity
+    this.radius = 15
+  }
+  draw(){
+    c.beginPath()
+    c.fillStyle = 'yellow'
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+    c.fill()
+    c.closePath()
+  }
+  update(){
+    this.draw()
+    this.position.x += this.position.x
+    this.position.y += this.position.y
+  }
+}
+
 const boundaries = []
+const player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width/2, y: Boundary.height + Boundary.height /2
+  },
+  velocity: {
+    x: 0, y:0
+  }
+})
 
 function createImage(src) {
   const image = new Image()
@@ -240,6 +265,31 @@ map.forEach((row, i) => {
   })
 })
 
-boundaries.forEach((boundary) => {
+function animate() {
+  requestAnimationFrame(animate)
+  boundaries.forEach(boundary => {
     boundary.draw()
+  })
+  player.draw()
+}
+animate()
+
+addEventListener('keydown', ({key}) => {
+  switch (key) {
+    case 'w':
+      player.velocity.y = -5
+      break;
+    case 's':
+      player.velocity.y = 5
+      break;
+    case 'd':
+      player.velocity.x = 5
+      break;
+    case 'a':
+      player.velocity.x = -5
+      break;
+  }
+  console.log(player.velocity)
 })
+
+
